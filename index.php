19f3c862079ea447d8a43f2d7f60c044ff2ec262
Florentino Dean Gas <gasflorentinodean@gmail.com>
2026-05-04 08:58:26 +0800

    Initial homepage commit

---
+++ index.php
@@ -0,0 +1,89 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="main">
+        <div class="nav-container">
+            <div class="nav-bar">
+                <ul>
+                    <li class="logo">iStar Tickets</li>
+                    <li class="nav-item">ABOUT</li>
+                    <li class="nav-item">HOME</li>
+                    <li class="nav-item">BOOK NOW</li>
+                    <li class="nav-item">LOGIN</li>
+                </ul>
+            </div>
+        </div>
+
+
+        <div class="content">
+            <div class="banner-container">
+                <h1>ARE YOU READY TO MEET YOUR <br> <span class="highlighted-text"> FAVORITE ARTISTS? </span></h1>
+            </div>
+
+        <center><h1 class="content-header">TOP ARTISTS</h1></center>
+            <div class="user-category-selection">
+
+
+                <div class="category-card">
+                    <div class="category-card-content">
+                        <div class="user-icon-container">
+                            <img src="assets/lany-logo.jpg" alt="user-icon" class="user-icon">
+                        </div>
+                        
+                        <div class="info-container">
+                            <h2>LANY - World Tour</h2>
+                            <p>Catch up with the music of LANY, live in Manila!</p>
+                        </div>
+
+                        <div class="link-container">
+                            <a href=""><img src="assets/right.png" alt="right-button" class="card-icons"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="category-card">
+                    <div class="category-card-content">
+                        <div class="user-icon-container">
+                            <img src="assets/ariana-logo.jpg" alt="user-icon" class="user-icon">
+                        </div>
+                        
+                        <div class="info-container">
+                            <h2>ARIANA GRANDE</h2>
+                            <p>Catch up with the music of Ariana, live in Manila!</p>
+                        </div>
+
+                        <div class="link-container">
+                            <a href=""><img src="assets/right.png" alt="right-button" class="card-icons"></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="category-card">
+                    <div class="category-card-content">
+                        <div class="user-icon-container">
+                            <img src="assets/taylor-logo.jpg" alt="user-icon" class="user-icon">
+                        </div>
+                        
+                        <div class="info-container">
+                            <h2>TAYLOR SWIFT</h2>
+                            <p>Catch up with the music of Taylor, live in Manila!</p>
+                        </div>
+
+                        <div class="link-container">
+                            <a href=""><img src="assets/right.png" alt="right-button" class="card-icons"></a>
+                        </div>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
