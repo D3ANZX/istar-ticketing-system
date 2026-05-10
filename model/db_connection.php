0b97dc5f935d24f640bce4e408d917bc4c15d652
BJ B. Saycon <sayconbj@gmail.com>
2026-05-10 20:12:02 +0800

    ver1 register beta (verify pls)

--- model/db_connection.php
+++ model/db_connection.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $db_username = "root";
     $db_password = "";
-    $dbname = "istardb";
+    $dbname = "istar"; // Change back to istardb
 
 
     //DB Connection init
